be41a5586c1d53122835f8df91ab9ae89d3db871
vitukjk <victorviniciusben612@gmail.com>
2025-04-28 01:21:27 -0300

    ShadowNuvaGeracion

---
+++ comandos.js
@@ -0,0 +1,18 @@
+/*
+COMANDOS DE INSTALAÇÃO DO SHADOW-BOT - BY: VITUKJK
+
+1°- pkg upgrade -y && pkg update -y && pkg install nodejs -y && pkg install nodejs-lts -y && pkg install ffmpeg -y && pkg install wget -y && pkg install git -y
+2°- pkg install npm
+3°- pkg install python
+4°- pkg install
+5°- termux-setup-storage
+6°- cd /sdcard/Download/ShadowV4.2
+(se esse não for, tenta o de baixo)
+cd /sdcard/ShadowV4.2
+7°- sh start.sh
+*/
+
+const vaiSeFuder = "Boa Sorte"
+// Espero q goste do bot. Qualquer dúvida, me chame no zap: +55 21 97198-3758
+// Um beijo, um abraço e um amasso do Vitu <3
+// Para adquirir a base completa, basta chamar o número acima :)
